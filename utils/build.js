5e3636b595c05d53565f2726ddc88483fb4a210b
Célestin Matte <celestin.matte@gmail.com>
2023-06-23 10:33:38 +0200

    Remove webpack-related files

--- utils/build.js
+++
@@ -1,9 +0,0 @@
-const webpack = require('webpack');
-const config = require('../webpack.config');
-
-delete config.chromeExtensionBoilerplate;
-
-webpack(
-  config,
-  (err) => { if (err) throw err; },
-);
